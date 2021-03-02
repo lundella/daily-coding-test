57a9a2b7c0c0cdb5960e29e3c6a6398f7451b665
lundella <mato7313@gmail.com>
2021-03-02 17:21:53 +0900

    Date method version

--- 2021-03-02.js
+++ 2021-03-02.js
@@ -1,10 +1,9 @@
 function solution(a, b) {
-  let answer = '';
-  const days = ['SUN','MON','TUE','WED','THU','FRI','SAT']
-  
-  const today = new Date('2016', a-1, b);
-  
-  answer = days[today.getDay()];
-  
+  let answer = ''; 
+  const today = new Date(2016, a-1, b);
+  const options = { weekday: 'short'};
+
+  answer = today.toLocaleDateString('en-US', options).toUpperCase();
+
   return answer;
 }
