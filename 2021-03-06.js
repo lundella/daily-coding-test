e6025da85a1e6d6e270dc350e64793ed9343f4a4
Juyoung Moon <mato7313@gmail.com>
2021-03-06 16:56:18 +0900

    second solution

--- 2021-03-06.js
+++ 2021-03-06.js
@@ -1,12 +1,10 @@
 function solution(arr, divisor) {
   var answer = [];
 
-  answer = arr.filter(element => element % divisor === 0)
+  answer = arr.filter(target => !(target % divisor) ? true : false).sort((a, b) => a - b);
 
   if (!answer.length) {
     answer.push(-1);
-  } else {
-    answer.sort((a, b) => a - b)
   }
 
   return answer;
