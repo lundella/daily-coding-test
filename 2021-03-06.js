768978f4afed45259c218fd5a93f042111fb6263
lundella <mato7313@gmail.com>
2021-03-08 15:44:50 +0900

    discriminant change

--- 2021-03-06.js
+++ 2021-03-06.js
@@ -1,7 +1,7 @@
 function solution(arr, divisor) {
   var answer = [];
 
-  answer = arr.filter(target => !(target % divisor) ? true : false).sort((a, b) => a - b);
+  answer = arr.filter(target => !(target % divisor)).sort((a, b) => a - b);
 
   if (!answer.length) {
     answer.push(-1);
