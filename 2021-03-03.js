ebf827b3fe67193e69f43ae2518e866c88d7503c
lundella <mato7313@gmail.com>
2021-03-09 13:28:14 +0900

    add self calculation

--- 2021-03-03.js
+++ 2021-03-03.js
@@ -1,3 +1,25 @@
+// https://programmers.co.kr/learn/courses/30/lessons/68935#
+
 function solution(n) {
-  return parseInt(n.toString(3).split('').reverse().join(''), 3);
+  let sum = '';
+  let target = n;
+  let result = [];
+  let iteration = true;
+  const base = 3;
+
+  while(iteration) {      
+    if(!parseInt(target/base)) {
+        iteration = false;
+    }
+      
+    result.push(target%base);
+    target = parseInt(target/base);
+  }
+  
+  sum = result.reduce((accu, value, index) => {
+    const power = (result.length-(index+1));
+    return accu + (Math.pow(base, power) * value);
+  }, 0);
+
+  return sum;
 }
