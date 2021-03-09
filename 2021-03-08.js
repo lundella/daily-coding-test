1273967a4ac39fd65a2626f943bfa5305e8ab478
lundella <mato7313@gmail.com>
2021-03-09 10:07:08 +0900

    second solution

--- 2021-03-08.js
+++ 2021-03-08.js
@@ -1,23 +1,14 @@
-// https://programmers.co.kr/learn/courses/30/lessons/12916
-
 function solution(s){
-  var answer = true;
-
-  var arrayString = s.split('');
   var count = 0;
-  
-  arrayString.forEach((char, index) => {
-      let standardChar = char.toLowerCase();
-      if(standardChar === 'p') {
+
+  s.toLowerCase().split('').forEach(char => {
+      if(char === 'p') {
           count++;
-      } else if(standardChar === 'y') {
+      }
+      if(char === 'y') {
           count--;
       }
   })
-  
-  if(count) {
-      answer = false;
-  }
 
-  return answer;
+  return (!count);
 }
