57217e2f0d7b7de6ba7747f02074768ffd5f4a53
lundella <mato7313@gmail.com>
2021-03-11 12:41:30 +0900

    second solution

--- 2021-03-11.js
+++ 2021-03-11.js
@@ -1,7 +1,14 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12919
 
 function solution(seoul) {
-    var answer = `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+    var answer = '';
     
+    for(let index = 0; index < seoul.length; index++) {
+      if(seoul[index] === 'Kim') {
+        answer = `김서방은 ${index}에 있다`;
+        break;
+      }
+    }
+
     return answer;
 }
