1682786db45b7962408c56d90b18d8a9a9bf3efe
lundella <mato7313@gmail.com>
2021-04-08 06:49:32 +0900

    initial solution

---
+++ 2021-04-07.js
@@ -0,0 +1,23 @@
+function solution(N, stages) {
+  const result = {};
+  const failRates = {};
+  let targetSum = 0;
+
+  stages.forEach(value => {
+      if(result[value] === undefined) {
+          result[value] = 0;
+      }
+      result[value] += 1;
+  })
+  
+  targetSum = (result[N+1])? result[N+1]: 0;
+  
+  for(let index = N; 0 < index; index--) {
+      const validCount = (result[index])? result[index]: 0; 
+      targetSum += validCount;
+
+      failRates[index] = (validCount/targetSum); 
+  }
+
+  return Object.keys(failRates).sort((a, b) => failRates[b] - failRates[a]).map(value => parseInt(value));;
+}
