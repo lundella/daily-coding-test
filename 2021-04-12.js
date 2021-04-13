44038e9f6d525cfc2325b9ac488391eb060178e0
lundella <mato7313@gmail.com>
2021-04-13 14:39:55 +0900

    solving

--- 2021-04-12.js
+++ 2021-04-12.js
@@ -1,2 +1,24 @@
-// https://programmers.co.kr/learn/courses/30/lessons/42587
+function solution(priorities, location) {
+  var answer = 0;
+  let iterationCount = 0;
+  const ordered = [];
+  const formatted = priorities.map((value, index) => {
+      return {[index] : value};
+  })
+  
+  // console.log(formatted);
+  
+  while(formatted.length) {
+      const target = formatted.shift();
 
+      if(Object.values(target)[0] < Math.max(...Object.values(formatted))) {
+          formatted.push(target);
+      } else {
+          ordered.push(Object.values(target)[0]);
+      }
+      console.log(formatted)
+  }
+  console.log(ordered);
+  
+  return answer;
+}
