9483f35e4cb1bbb50285a95ab5e8bb1e3cc327ea
lundella <mato7313@gmail.com>
2021-04-16 13:44:56 +0900

    initial solution

--- 2021-04-12.js
+++ 2021-04-12.js
@@ -1,24 +1,24 @@
-function solution(priorities, location) {
-  var answer = 0;
-  let iterationCount = 0;
-  const ordered = [];
-  const formatted = priorities.map((value, index) => {
-      return {[index] : value};
-  })
-  
-  // console.log(formatted);
-  
-  while(formatted.length) {
-      const target = formatted.shift();
+// https://programmers.co.kr/learn/courses/30/lessons/42587?language=javascript
 
-      if(Object.values(target)[0] < Math.max(...Object.values(formatted))) {
-          formatted.push(target);
-      } else {
-          ordered.push(Object.values(target)[0]);
-      }
-      console.log(formatted)
-  }
-  console.log(ordered);
-  
-  return answer;
+function solution(priorities, location) {
+    const maxFinder = Object.assign([], priorities);
+    const ordered = [];
+    const formatted = priorities.map((value, index) => {
+        return {[index] : value};
+    })
+
+    while(formatted.length) {
+        const target = formatted.shift();
+        const value = Object.values(target)[0];
+        
+        if(value < Math.max(...maxFinder)) {
+            formatted.push(target);
+        } else {
+            maxFinder.splice(maxFinder.findIndex(element => element === value), 1);
+
+            ordered.push(target);
+        }
+    }
+    
+    return ordered.findIndex(element => element[location])+1;
 }
