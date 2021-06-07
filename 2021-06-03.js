c2fdcec1459183c3ec2355d1520c838f89d45aa9
lundella <mato7313@gmail.com>
2021-06-07 13:32:25 +0900

    (완료)  크레인 인형뽑기 게임 - 성능 문제 해결

--- 2021-06-03.js
+++ 2021-06-03.js
@@ -2,28 +2,27 @@
 
 function solution(board, moves) {
   let answer = 0;
-  const stackBoard = [[], [], [], [], []];
   const basket = [];
   
-  board.forEach((value, index) => {
-      value.forEach((figure, index) => {
-          if(value[index]) {
-              stackBoard[index].splice(0, 0, value[index]);
-          }
-      })
-  })
-  
   moves.forEach(value => {
-      const picked = stackBoard[(value-1)].pop();
-      if(picked) {
-          if(basket[basket.length-1] === picked) {
-              basket.pop();
-              answer += 2;
-          } else {
-              basket.push(picked);    
+      const place = value-1;
+      
+      for(let index = 0; index < board.length; index++) {
+          const picked = board[index][place];
+          
+          if(picked) {
+              board[index][place] = 0;
+
+              if(basket[basket.length-1] === picked ) {
+                  basket.pop();
+                  answer += 2;
+              } else {
+                  basket.push(picked);
+              }
+              break;
           }
       }
-  })    
+  })
   
   return answer;
 }
